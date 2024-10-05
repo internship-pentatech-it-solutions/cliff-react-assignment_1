58e9bc001db715c8aeb63d676cc29a76158e0054
clifforddonk <clifforddonk@gmail.com>
2024-10-05 19:49:58 +0000

    first commit

---
+++ src/routes/Services.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../Components/Navbar";
+const Services = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="text">Services</div>
+    </div>
+  );
+};
+
+export default Services;
